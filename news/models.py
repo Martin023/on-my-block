05926588ded83bf3310f25885aedd58d73da6165
martin023 <mainamartin023@gmail.com>
2022-06-20 16:07:02 +0300

    This commit will add various model methods

--- news/models.py
+++ news/models.py
@@ -21,6 +21,28 @@ class Neighbourhood(models.Model):
     def __str__(self):
         return self.neighbourhood_name
 
+    def create_neigborhood(self):
+        self.save()
+
+    def delete_neigborhood(self):
+        self.delete()
+
+    @classmethod
+    def find_neigborhood(cls,neigborhood):
+        searched_hoods = cls.objects.filter(neighbourhood_name__icontains=neigborhood)
+        return searched_hoods
+
+    def update_neighborhood(self,neighbourhood_name,neighbourhood_location,occupants_count,admin):
+        self.neighbourhood_name=neighbourhood_name
+        self.neighbourhood_location=neighbourhood_location
+        self.occupants_count=occupants_count
+        self.admin=admin
+
+        self.save()
+
+    def update_occupants():
+        pass
+
 
 # class User(models.Model):
 #     user_name = models.CharField(max_length=30)
@@ -37,3 +59,23 @@ class Business(models.Model):
 
     def __str__(self):
         return self.business_name
+
+    def create_business(self):
+        self.save()
+
+    def delete_business(self):
+        self.delete()
+
+    @classmethod
+    def find_busines(cls,business_name):
+        business =cls.objects.filter(business_id__icontains= business_name) 
+        return business
+
+    def update_business(self,business_name,neighbourhood_name,business_email,business_image):
+        self.business_name=business_name
+        self.business_image=business_image
+        self.business_email=business_email
+        self.neighbourhood_name=neighbourhood_name
+
+        self.save()
+    
